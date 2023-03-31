a7459fae9da42471680d3bb303f712d1acedb54b
Aakash Agarwal <aakash@ip-172-19-25-168.ap-south-1.compute.internal>
2023-03-31 11:44:14 +0530

    fixing pypi issue

--- devlibx_avro_helper/month_data_v1.py
+++ devlibx_avro_helper/month_data_v1.py
@@ -11,7 +11,7 @@ def get_keys_for_current_week_for_day_aggregation_from_given_time(time):
     """
     result = []
     end = time
-    start = time.replace(minute=0, hour=0, second=0, microsecond=0) - timedelta(days=time.weekday())
+    start = time.replace(minute=0, hour=0, second=0, microsecond=0)-timedelta(days=time.weekday())
     while start <= end:
         result.append("{}-{}".format(start.month, start.day))
         start = start + timedelta(days=1)
